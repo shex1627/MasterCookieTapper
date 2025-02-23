f54ffb6124da15d94a5d4906e0eb18b0d395a02a
shicheng <alistar.huang@gmail.com>
2025-02-22 18:18:43 -0800

    Initial Commit

---
+++ MasterCookieTapper Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MasterCookieTapper Watch App
+//
+//  Created by Shicheng Huang on 2/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MasterCookieTapper Watch App/MasterCookieTapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterCookieTapperApp.swift
+//  MasterCookieTapper Watch App
+//
+//  Created by Shicheng Huang on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MasterCookieTapper_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
